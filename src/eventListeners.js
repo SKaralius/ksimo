33b84a845ac4d97262ce4cc3fe4638265a025ca6
SKaralius <k.simonas.k@gmail.com>
2020-09-27 16:45:53 +0300

    Modal layout.

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -1,11 +1,42 @@
+import { selectContent } from "./featureModalContent";
+
 export function listenForEvents(setUpContext, app) {
   window.addEventListener("load", function () {
+    const modal = document.getElementsByClassName("feature-modal")[0];
     document.getElementsByTagName("html")[0].style.cssText = `
                 visibility: visible;
                 height: unset;
                 overflow: auto`;
     document.getElementsByClassName("spinner-container")[0].style.cssText = `
           display:none`;
+    // Add show modal function to buttons
+    const buttons = document.getElementsByTagName("button");
+    let closeButtonRef;
+    for (let button of buttons) {
+      if (button.className !== "modal-close-button") {
+        button.onclick = () => {
+          modal.style.cssText = `
+        display: block
+        `;
+          modal.innerHTML += selectContent(button.id);
+          closeButtonRef.style.cssText = `
+          display: block;
+          `;
+        };
+        // Close button in the modal
+        // gets close modal function
+      } else {
+        closeButtonRef = button;
+        button.onclick = () => {
+          modal.style.cssText = `
+        display: none
+        `;
+          closeButtonRef.style.cssText = `
+          display: none
+          `;
+        };
+      }
+    }
     setUpContext();
     app();
   });
---
+++ src/featureModalContent.js
@@ -0,0 +1,4 @@
+export function selectContent(id) {
+  console.log("adds content for", id);
+  return `<img src="../src/assets/images/scales.jpg" alt="">`;
+}
